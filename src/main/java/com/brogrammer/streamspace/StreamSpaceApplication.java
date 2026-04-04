d326f8d4d3fc55dbd97ec0fec0681133ea50c765
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2026-04-04 20:42:44 +0530

    Remove caching annotation from StreamSpaceApplication

--- src/main/java/com/brogrammer/streamspace/StreamSpaceApplication.java
+++ src/main/java/com/brogrammer/streamspace/StreamSpaceApplication.java
@@ -2,12 +2,10 @@ package com.brogrammer.streamspace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
-@EnableCaching
 @EnableScheduling
 @SpringBootApplication
 public class StreamSpaceApplication {
